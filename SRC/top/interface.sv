36cb54bba0c1f66a65a705eadd79aee99481c6ef
Deniz Guzel <deniz.guzel@yongatek.com>
2024-07-24 12:51:36 +0300

    Revising project hierarchy, methodology, coding style

--- SRC/top/interface.sv
+++
@@ -1,8 +0,0 @@
-interface s_interface(input logic i_clk, input logic i_rst);
-
-  logic [15:0] i_memData;
-  logic [15:0] o_memData;
-  logic [15:0] o_memAddr;
-  logic o_memWrEnable;
-
-endinterface: s_interface
--- utils/config_macro.svh
+++
@@ -1,4 +0,0 @@
-`define get_config(cfg_type, cfg_handle, cfg_name) \
-if(``cfg_handle`` == null) \
-	if(!uvm_config_db #(``cfg_type``)::get(this, "", ``cfg_name``, ``cfg_handle``)) \
-		`uvm_fatal("CONFIG_LOAD", $sformatf("Cannot get() configuration %s from uvm_config_db. Have you set() it?", ``cfg_name``))
